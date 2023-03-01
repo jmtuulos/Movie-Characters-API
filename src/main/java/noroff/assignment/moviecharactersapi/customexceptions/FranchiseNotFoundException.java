94184f3bcb790b2e7f7bf3205adcdb4a5d493302
jetrosa <jetro.saarti@gmail.com>
2023-03-01 03:03:12 +0200

    added custom exceptions (not found)

---
+++ src/main/java/noroff/assignment/moviecharactersapi/customexceptions/FranchiseNotFoundException.java
@@ -0,0 +1,12 @@
+package noroff.assignment.moviecharactersapi.customexceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class FranchiseNotFoundException extends RuntimeException {
+    public FranchiseNotFoundException(int id) {
+        super("Franchise does not exist with ID: " + id);
+    }
+
+}
