cf2c1076731638436d35d431c3a7ce7a08cf46ee
jetrosa <jetro.saarti@gmail.com>
2023-03-01 03:04:28 +0200

    modified service implementations to use db repositories

--- src/main/java/noroff/assignment/moviecharactersapi/services/FranchiseServiceImpl.java
+++ src/main/java/noroff/assignment/moviecharactersapi/services/FranchiseServiceImpl.java
@@ -1,59 +1,82 @@
 package noroff.assignment.moviecharactersapi.services;
 
+import noroff.assignment.moviecharactersapi.customexceptions.FranchiseNotFoundException;
 import noroff.assignment.moviecharactersapi.models.Character;
 import noroff.assignment.moviecharactersapi.models.Franchise;
 import noroff.assignment.moviecharactersapi.models.Movie;
+import noroff.assignment.moviecharactersapi.repositories.FranchiseRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
 
 @Service
 public class FranchiseServiceImpl implements FranchiseService {
-    @Override
-    public Franchise findById(Integer integer) {
-        return null;
+    private final FranchiseRepository franchiseRepository;
+    private final Logger logger = LoggerFactory.getLogger(FranchiseServiceImpl.class);
+
+    public FranchiseServiceImpl(FranchiseRepository franchiseRepository) {
+        this.franchiseRepository = franchiseRepository;
     }
 
     @Override
-    public Collection<Franchise> findAll() {
-        return null;
+    public Franchise findById(Integer id) {
+        return franchiseRepository.findById(id)
+                .orElseThrow(() -> new FranchiseNotFoundException(id));
+    }
+
+    @Override
+    public List<Franchise> findAll() {
+        List<Franchise> franchises = franchiseRepository.findAll();
+        return franchises;
     }
 
     @Override
     public Franchise add(Franchise entity) {
-        return null;
+        Franchise franchise = franchiseRepository.save(entity);
+        return franchise;
     }
 
     @Override
     public Franchise update(Franchise entity) {
-        return null;
+        Franchise franchise = franchiseRepository.save(entity);
+        return franchise;
     }
 
     @Override
-    public void deleteById(Integer integer) {
-
+    @Transactional
+    public void deleteById(Integer id) {
+        if (franchiseRepository.existsById(id)) {
+            Franchise franchise = franchiseRepository.findById(id).get();
+            franchise.getMovies().forEach(m -> m.setFranchise(null));
+            franchiseRepository.delete(franchise);
+        } else
+            logger.warn("No franchise exists with ID: " + id);
     }
 
     @Override
-    public void delete(Franchise entity) {
-
+    public void delete(Franchise franchise) {
+        franchise.getMovies().forEach(m -> m.setFranchise(null));
+        franchiseRepository.delete(franchise);
     }
 
     @Override
     public void updateMovies(int franchiseId, int[] movieId) {
-
+        //todo
     }
 
     @Override
     public Collection<Movie> getMovies(int franchiseId) {
-
+        //todo
         return List.of(new Movie[0]);
     }
 
     @Override
     public Collection<Character> getCharacters(int franchiseId) {
-
+        //todo
         return List.of(new Character[0]);
     }
 }
--- src/main/java/noroff/assignment/moviecharactersapi/services/MovieServiceImpl.java
+++ src/main/java/noroff/assignment/moviecharactersapi/services/MovieServiceImpl.java
@@ -1,7 +1,11 @@
 package noroff.assignment.moviecharactersapi.services;
 
+import noroff.assignment.moviecharactersapi.customexceptions.MovieNotFoundException;
 import noroff.assignment.moviecharactersapi.models.Character;
 import noroff.assignment.moviecharactersapi.models.Movie;
+import noroff.assignment.moviecharactersapi.repositories.MovieRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -9,44 +13,62 @@ import java.util.List;
 
 @Service
 public class MovieServiceImpl implements MovieService {
+    private final MovieRepository movieRepository;
+    private final Logger logger = LoggerFactory.getLogger(MovieServiceImpl.class);
+
+    public MovieServiceImpl(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     @Override
-    public Movie findById(Integer integer) {
-        return null;
+    public Movie findById(Integer id) {
+        return movieRepository.findById(id)
+                .orElseThrow(() -> new MovieNotFoundException(id));
     }
 
     @Override
     public Collection<Movie> findAll() {
-        return null;
+        List<Movie> movies = movieRepository.findAll();
+        return movies;
     }
 
     @Override
     public Movie add(Movie entity) {
-        return null;
+        Movie movie = movieRepository.save(entity);
+        return movie;
     }
 
     @Override
     public Movie update(Movie entity) {
-        return null;
+        Movie movie = movieRepository.save(entity);
+        return movie;
     }
 
     @Override
-    public void deleteById(Integer integer) {
-
+    public void deleteById(Integer id) {
+        if (movieRepository.existsById(id)) {
+            Movie movie = movieRepository.findById(id).get();
+            //movie.getCharacters().forEach(c -> c.setMovie(null));
+            //todo: remove movie from characters
+            movieRepository.delete(movie);
+        } else
+            logger.warn("No movie exists with ID: " + id);
     }
 
     @Override
-    public void delete(Movie entity) {
-
+    public void delete(Movie movie) {
+        //todo: remove movie from characters
+        movieRepository.delete(movie);
     }
 
     @Override
     public void updateCharacters(int movieId, int[] characterId) {
-
+        //todo
     }
 
     @Override
     public Collection<Character> getCharacters(int movieId) {
-
+        //todo
         return List.of(new Character[0]);
     }
 }
