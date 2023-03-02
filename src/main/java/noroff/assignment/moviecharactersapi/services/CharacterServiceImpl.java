8c7a665ada91d19c1c75208ee4abf9f78367c744
jetrosa <jetro.saarti@gmail.com>
2023-03-02 18:09:52 +0200

    Fix: removed references that prevented character deletion (many-to-many constraint)

--- src/main/java/noroff/assignment/moviecharactersapi/services/CharacterServiceImpl.java
+++ src/main/java/noroff/assignment/moviecharactersapi/services/CharacterServiceImpl.java
@@ -2,6 +2,7 @@ package noroff.assignment.moviecharactersapi.services;
 
 import noroff.assignment.moviecharactersapi.customexceptions.CharacterNotFoundException;
 import noroff.assignment.moviecharactersapi.customexceptions.MovieNotFoundException;
+import noroff.assignment.moviecharactersapi.models.Movie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import noroff.assignment.moviecharactersapi.repositories.CharacterRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CharacterServiceImpl implements CharacterService {
@@ -45,8 +47,7 @@ public class CharacterServiceImpl implements CharacterService {
     @Transactional
     public void deleteById(Integer id) {
         if (characterRepository.existsById(id)) {
-            Character chara = characterRepository.findById(id).get();
-            characterRepository.delete(chara);
+            delete(characterRepository.findById(id).get());
         } else
             logger.warn("No character exists with ID: " + id);
     }
@@ -59,6 +60,12 @@ public class CharacterServiceImpl implements CharacterService {
 
     @Override
     public void delete(Character character) {
+        Set<Movie> movies = character.getMovies();
+        movies.forEach(mov ->
+                mov.getCharacters()
+                        .removeIf(r -> r.getId() == character.getId()));
+        movies.clear();
+
         characterRepository.delete(character);
     }
 }
