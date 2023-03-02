a08a6de4c9a32e4cdaa82b1702a54b28d08a0f90
jetrosa <jetro.saarti@gmail.com>
2023-03-02 15:43:20 +0200

    Fix: switched joinColumns and inverseJoinColumns that were in the wrong order

--- src/main/java/noroff/assignment/moviecharactersapi/models/Character.java
+++ src/main/java/noroff/assignment/moviecharactersapi/models/Character.java
@@ -14,15 +14,15 @@ public class Character {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name = "full_name")
-    private String name;
+    private String fullName;
     private String alias;
     private String gender;
     private String photoUrl;
     @ManyToMany
     @JoinTable(
             name = "movie_characters",
-            joinColumns = {@JoinColumn(name = "movie_id")},
-            inverseJoinColumns = {@JoinColumn(name = "character_id")}
+            joinColumns = {@JoinColumn(name = "character_id")},
+            inverseJoinColumns = {@JoinColumn(name = "movie_id")}
     )
     private Set<Movie> movies;
 }
