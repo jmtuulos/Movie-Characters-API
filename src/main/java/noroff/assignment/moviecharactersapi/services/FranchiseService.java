19b0ec0ee8e874505a5855dc6e0b2954e6f05dd7
jetrosa <jetro.saarti@gmail.com>
2023-03-02 18:36:07 +0200

    merge cleanup - fixed data type

--- src/main/java/noroff/assignment/moviecharactersapi/services/FranchiseService.java
+++ src/main/java/noroff/assignment/moviecharactersapi/services/FranchiseService.java
@@ -3,6 +3,8 @@ package noroff.assignment.moviecharactersapi.services;
 import noroff.assignment.moviecharactersapi.models.Franchise;
 import noroff.assignment.moviecharactersapi.models.Movie;
 import noroff.assignment.moviecharactersapi.models.Character;
+
+import java.util.List;
 import java.util.Set;
 
 public interface FranchiseService extends CrudService<Franchise, Integer> {
@@ -10,5 +12,5 @@ public interface FranchiseService extends CrudService<Franchise, Integer> {
 
     Set<Movie> getMovies(int franchiseId);
 
-    Set<Character> getCharacters(int franchiseId);
+    List<Character> getCharacters(int franchiseId);
 }
